e54028e8e7d69c3e51e71fd4f3b77788759ace55
Nir Shmueli <nirshmueli2@gmail.com>
2024-04-11 18:07:50 +0300

    changed

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,137 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] array = new int[] { 0, 1, 2, 4, 5, 3, 8, 9, 6 };
+        int ans = findMissingInt(array);
+        System.out.println(ans);
+
+        int[] otherarray = new int[] { 3, 1, 23, 4, 52, 3, 3, 9, 52, 100, 101 };
+        int ans2 = secondMaxValue(otherarray);
+        System.out.println(ans2);
+
+        int[] array1 = new int[] { 0, 1, 2, 2, 3, 4 };
+        int[] array2 = new int[] { 2, 1, 0, 4, 3, 5 };
+        boolean ans3 = containsTheSameElements(array1, array2);
+        System.out.println(ans3);
+
+        boolean ans4 = isSorted(array1);
+        System.out.println(ans4);
+
     }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
+
+    public static int findMissingInt(int[] array) {
+        int curNum = -1;
+        int prevchar = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                curNum = 0;
+            }
+        }
+        if (curNum != 0) {
+            return 0;
+        }
+        for (int j = 0; j < array.length; j++) {
+            for (int u = 0; u < array.length; u++) {
+                if (array[u] == curNum + 1) {
+                    curNum++;
+                    break;
+                }
+            }
+            if (prevchar == curNum) {
+                return (prevchar + 1);
+            }
+            prevchar++;
+        }
         return -1;
     }
 
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+    public static int secondMaxValue(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int[] newarray = remove(array, max);
+        int secondmax = newarray[0];
+        for (int i = 1; i < newarray.length; i++) {
+            if (newarray[i] > secondmax) {
+                secondmax = newarray[i];
+            }
+        }
+        return secondmax;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+    public static int[] remove(int[] arr, int element) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                count++;
+            }
+        }
+        int[] ans = new int[arr.length - count];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != element) {
+                ans[index] = arr[i];
+                index++;
+            }
+        }
+        return ans;
     }
 
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
+        boolean side1 = containedintheother(array1, array2);
+        boolean side2 = containedintheother(array2, array1);
+        boolean both = side1 && side2;
+        return both;
+    }
+
+    public static boolean containedintheother(int[] array1, int[] array2) {
+        boolean ans = false;
+        for (int i = 0; i < array1.length; i++) {
+            for (int u = 0; u < array2.length; u++) {
+                if (array1[i] == array2[u]) {
+                    ans = true;
+                    break;
+                } else {
+                    ans = false;
+                }
+            }
+            if (!ans) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    public static boolean isSorted(int[] array) {
+        boolean DecreasingTest = isDecreasing(array);
+        boolean increasingTest = isIncreasing(array);
+        boolean answer = DecreasingTest || increasingTest;
+        return answer;
+
+    }
+
+    public static boolean isDecreasing(int[] array) {
+        boolean Decreasing = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                Decreasing = false;
+                break;
+            }
+        }
+        return Decreasing;
+    }
+
+    public static boolean isIncreasing(int[] array) {
+        boolean increasing = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                increasing = false;
+                break;
+            }
+        }
+        return increasing;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,121 @@
 public class StringOps {
     ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
+    ////// ///////
+    ////// Reminder: ///////
+    ////// allowed methods ///////
+    ////// ///////
+    ////// 1.charAt(int index) ///////
+    ////// 2.length() ///////
+    ////// 3.substring(int start) ///////
+    ////// 4.substring(int start,int ends) ///////
+    ////// 5.indexOf(String str) ///////
+    ////// ///////
+    ////// The rest are not allowed ! ///////
+    ////// if you want to use a different ///////
+    ////// method, and you can implement ///////
+    ////// it using material from the course ///////
+    ////// you need to implement a version of ///////
+    ////// the function by yourself. ///////
+    ////// ///////
+    ////// see example for substring ///////
+    ////// in Recitation 3 question 5 ///////
+    ////// ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String input1 = "OlOnel two tHRee world";
+        int[] answer = new int[1000];
+        answer = allIndexOf(input1, 'l');
+        System.out.print("{ ");
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] + " ");
+        }
+        System.out.println("}");
+
+        // System.out.println(answer);
+
+        String ans1 = capVowelsLowRest(input1);
+        System.out.println(ans1);
+
+        String input2 = "   Intro  to coMPUter sCIEncE";
+        String ans2 = camelCase(input2);
+        System.out.println(ans2);
+
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest(String string) {
+        String LowVowels = "aeiou";
+        String CapVowels = "AEIOU";
+        String answer = "";
+        for (int i = 0; i < string.length(); i++) {
+            char cur = string.charAt(i);
+            if (cur > 96 && cur < 123) { // lowercase
+                for (int u = 0; u < LowVowels.length(); u++) {
+                    if (cur == LowVowels.charAt(u)) {
+                        cur -= 32; // to up
+                        break;
+                    }
+                }
+            } else if (cur < 97 && cur > 64) { // uppercase
+                boolean isVowel = false;
+                for (int u = 0; u < CapVowels.length(); u++) {
+                    if (cur == CapVowels.charAt(u)) {
+                        isVowel = true;
+                    }
+                }
+                if (!isVowel) {
+                    cur += 32; // to low
+                }
+            }
+            answer += cur;
+        }
+        return answer;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        String Newword = "";
+        String Finalword = "";
+        for (int i = 0; i < string.length(); i++) {
+            char cur = string.charAt(i);
+            if (cur < 97 && cur > 64) {
+                cur += 32;
+                Newword += cur;
+            } else if (cur > 96 && cur < 123) {
+                Newword += cur;
+            }
+        }
+
+        for (int u = 0; u < Newword.length(); u++) {
+            char curNew = Newword.charAt(u);
+            if (u == 0) {
+                curNew -= 32;
+            }
+            Finalword += curNew;
+        }
+
+        return Finalword;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf(String string, char chr) {
+        int arraysizecount = arraysize(string, chr);
+        int[] array = new int[arraysizecount];
+        int arrayIndex = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                array[arrayIndex] = i;
+                arrayIndex++;
+            }
+        }
+        return array;
+    }
+
+    public static int arraysize(String string, char chr) {
+        // computes the array size
+        int arraysizecount = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                arraysizecount++;
+            }
+        }
+        return arraysizecount;
     }
 }
